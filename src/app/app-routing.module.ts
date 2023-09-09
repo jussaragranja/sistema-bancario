5008dc03c90ec1b6d08134e7d87a82d034be16e6
lab10maql25 <lab10maql25@debian-BULLSEYE-live-builder-AMD64>
2023-09-09 14:23:13 -0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListagemClientesComponent } from './components/listagem-clientes/listagem-clientes.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"clientes",
+    component: ListagemClientesComponent,
+    children: [
+      {
+        path: "listagem",
+        component: ListagemClientesComponent
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { MaterialModule } from './shared/material/material.module';
+import { ListagemClientesComponent } from './components/listagem-clientes/listagem-clientes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ListagemClientesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
